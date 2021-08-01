6908f210a68757865f6df1b4e95862dbf699af48
shaik zakir <84846163+shaikzakirr@users.noreply.github.com>
2021-08-01 19:50:35 +0530

    Updated : Tested and modified

--- HitCoreJavaProjectWorks/src/hit/emailproject/Mailer.java
+++ HitCoreJavaProjectWorks/src/hit/emailproject/Mailer.java
@@ -1,7 +1,7 @@
 /**
- * @Author		: Tahir Shaik
+ * @Author		: Zakir shaik
  * @Date		: 11/07/21
- * @E-Mail		: tahirsk1920@gmail.com
+ * @E-Mail		: zakscoding@gmail.com
  * @FileName	: Mailer.java
  * @Description	: This file contains the Main Source Code
  * @Dependancies: This file Requires SendEmail.java to execute the program
@@ -14,12 +14,9 @@ public class Mailer {
 		String subject ="A Quick Hello & Thanks for Visiting Our Website !!!";
 		String textBody = "Greetings To You.\n\n"
 				+ "We're Welcoming You!!.\n"
-				+ "Join our Github and Connect/Follow us on LinkedIN !!!!\n"
-				+ "Our GitHub URL: https://github.com/TahirShaik\n"
-				+ "LinkedIN URL: https://www.linkedin.com/in/tahir1920 \n"
-				+ "Visit Our Portfolio: https://tahirshaik.github.io/WebDevlopment-/FirstPortfoliowebDesign/index.html"
-				+ "\n\n\nThanks & Regards,\nTahir Shaik,\nE-Mail: tahirsk1920@gmail.com ,\n"
-				+ "Phone No: +91-9177327808.";
+				+ "Our GitHub URL: https://github.com/shaikzakirr\n"
+				+ "\n\n\nThanks & Regards,\nShaik Zakir,\nE-Mail: zakscoding@gmail.com ,\n"
+				+ "Phone No: +91-8500754867.";
 	
 		String fromMail = "fromMain@gmail.com";
 		String password = "password";
